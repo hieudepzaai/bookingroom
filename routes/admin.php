5ecddfd92e1894feb6cc3a3d780bfc42507013b9
MR dat <mynameisdat6@gmail.com>
2022-03-29 10:15:14 +0700

    add post controller

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\BackEnd\address\DistrictController;
-
-Route::get('district', [DistrictController::class , 'index']);
+Route::get('district', [\App\Http\Controllers\address\DistrictController::class , 'index']);
 //Route::get('district', [\App\Http\Controllers\address\DistrictController::class , 'getAll']);
-Route::get('district/{id}', [DistrictController::class , 'getById']);
+Route::get('districts/{id}', [\App\Http\Controllers\address\DistrictController::class , 'getById']);
